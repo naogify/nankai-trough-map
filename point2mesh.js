6469cc544a2be58f50ffb1cd8b1828e499487cf2
naoki <n.globe.us@gmail.com>
2022-10-31 10:01:50 +0900

    some fix

--- point2mesh.js
+++ point2mesh.js
@@ -1,21 +1,13 @@
 const meterstodegrees = require("meterstodegrees");
 const bigDecimal = require('js-big-decimal');
 
-// // Base Latitude at Wakayama, Japan
-// const latitude = 34;
-// const meters = 10
-
 // function to convert point to polygon
 function point2mesh(lat, lon, meters) {
 
     const baseLat = parseInt(lat);
-    console.log(baseLat);
 
     const latDegreesOf10m = meterstodegrees.latDegrees(baseLat, meters)
     const lonDegreesOf10m = meterstodegrees.lonDegrees(baseLat, meters)
-
-    console.log(latDegreesOf10m);
-    console.log(lonDegreesOf10m);
 
     let lon1 = bigDecimal.add(`${lon}`, `${lonDegreesOf10m}`);
     let lat1 = bigDecimal.subtract(`${lat}`, `${latDegreesOf10m}`);
@@ -25,11 +17,6 @@ function point2mesh(lat, lon, meters) {
     lon = parseFloat(lon);
     lat1 = parseFloat(lat1);
     lon1 = parseFloat(lon1);
-
-    console.log(lat);
-    console.log(lon);
-    console.log(lat1);
-    console.log(lon1);
 
     var coordinates = [
         [
